7b1c256557733dcd6f90f130a1485074928db474
Abdulrahman <abdulrahman.m.hallak.293@gmail.com>
2023-03-27 22:47:01 +0200

    Added qsort algorithm

---
+++ QuickSortAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms;
+internal class QuickSortAlgorithm
+{
+    public static IEnumerable<int> QuickSort(IEnumerable<int> input)
+    {
+        if(input.Count() <= 2) return input;
+
+        var pivot = input.First();
+        var greater = input.Skip(1).Where( x => x > pivot);
+        var less = input.Skip(1).Where( x => x <= pivot );
+        
+        return QuickSort(less).Concat(new List<int>() { pivot}).Concat(QuickSort(greater));
+    }
+}
