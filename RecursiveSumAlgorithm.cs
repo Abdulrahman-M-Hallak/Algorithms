2e42ed7858ff6f4fac7c3442057ac220c8c48832
Abdulrahman <abdulrahman.m.hallak.293@gmail.com>
2023-03-27 22:46:27 +0200

    Added the recursive sum algorithm

---
+++ RecursiveSumAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithms;
+internal static class RecursiveSumAlgorithm
+{
+    public static int Sum(IEnumerable<int> list)
+    {
+        if(!list.Any())
+            return 0;
+        return list.First() + Sum(list.Skip(1));
+    }
+    public static int NumberOfElements(IEnumerable<int> list)
+    {
+        if(!list.Any()) return 0;
+        
+        return 1 + NumberOfElements(list.Skip(1));
+    }
+}
