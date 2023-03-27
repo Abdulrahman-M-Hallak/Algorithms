aa67b527e894fecf69ba849ff9ee3d52de5f6ff1
Abdulrahman <abdulrahman.m.hallak.293@gmail.com>
2023-03-27 18:14:38 +0200

    Added a recursive sum function

---
+++ TheSumFuntion.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms;
+internal static class TheSumFuntion
+{
+    public static int Sum(IEnumerable<int> list)
+    {
+        if(!list.Any())
+            return 0;
+        return list.First() + Sum(list.Skip(1));
+    }
+}
